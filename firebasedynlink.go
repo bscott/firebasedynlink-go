d67e8dde54f96659e68b6ac8940f06ddb363b0bf
Brian Scott <brian@bscott.io>
2018-05-03 03:21:30 +0000

    Continue to fillout the base package with Request funcs

--- firebasedynlink.go
+++ firebasedynlink.go
@@ -9,6 +9,7 @@ type Client struct {
   httpClient *http.Client
 }
 
+// Request type
 type Request struct {
 	LongDynamicLink string `json:"longDynamicLink"`
 	Suffix          struct {
@@ -16,7 +17,44 @@ type Request struct {
 	} `json:"suffix"`
 }
 
+// CreateLink function method
 func (c *Client) CreateLink(longURL string) {
+  req, err := c.newRequest("POST", "/v1/shortlinks", nil)
+  
+  
   
 }
 
+// Creates a new request
+func (c *Client) newRequest(method, path string, body interface{}) (*http.Request, error) {
+    rel := &url.URL{Path: path}
+    u := c.BaseURL.ResolveReference(rel)
+    var buf io.ReadWriter
+    if body != nil {
+        buf = new(bytes.Buffer)
+        err := json.NewEncoder(buf).Encode(body)
+        if err != nil {
+            return nil, err  
+        }
+    }
+    req, err := http.NewRequest(method, u.String(), buf)
+    if err != nil {
+        return nil, err
+    }
+    if body != nil {  
+        req.Header.Set("Content-Type", "application/json") 
+    }
+    req.Header.Set("Accept", "application/json")
+    req.Header.Set("User-Agent", c.UserAgent)
+    return req, nil
+}
+
+func (c *Client) do(req *http.Request, v interface{}) (*http.Response, error) {
+    resp, err := c.httpClient.Do(req)
+    if err != nil {
+        return nil, err
+    }
+    defer resp.Body.Close()
+    err = json.NewDecoder(resp.Body).Decode(v)
+    return resp, err
+}
